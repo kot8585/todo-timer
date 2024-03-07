07ebc7cf2eddacfb24d84545f54f81fbad4bc73d
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2024-03-07 14:21:31 +0900

    bottomNavigation에 아이콘 설정

--- src/navigation/BottomTaps.tsx
+++ src/navigation/BottomTaps.tsx
@@ -2,15 +2,44 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import MyScreen from '../screens/MyScreen';
 import TimelineScreen from '../screens/TimelineScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTaps() {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="홈" component={HomeScreen} />
-      <Tab.Screen name="타임라인" component={TimelineScreen} />
-      <Tab.Screen name="마이" component={MyScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: '홈',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Timeline"
+        component={TimelineScreen}
+        options={{
+          title: '타임라인',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="view-timeline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="My"
+        component={MyScreen}
+        options={{
+          title: '마이',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="person" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
