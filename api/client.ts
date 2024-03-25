b3c76f4afd17996c8cac2cb869784ae3cd9c70bb
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2024-03-25 19:08:50 +0900

    android simulator에서는 axios network error뜨길래 설정해줌. ios시뮬레이터에서는 잘 됐었다

--- api/client.ts
+++ api/client.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import {Platform} from 'react-native';
 
-const baseURL = 'http://localhost:8080';
+// const baseURL = 'http://localhost:8080';
+// const baseURL = 'http://10.0.2.2:8080';
+const baseURL =
+  Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:8080';
 
 const client = axios.create({baseURL});
 
