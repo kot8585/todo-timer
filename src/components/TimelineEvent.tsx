d3cca141f7a6a5b61f1f23ea0f45667f599b1f7e
HyoJeong Moon <57999419+kot8585@users.noreply.github.com>
2024-04-06 01:20:24 +0900

    timelineEvent UI 수정
    - 시간이 이어질 경우 양옆에 border가 없도록
    - 가장 넓이가 긴 타임라인한테 text출력하도록 하기

--- src/components/TimelineEvent.tsx
+++ src/components/TimelineEvent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, Text} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {TimelineType} from '../api/types';
 
 type TimelineEventProps = {
@@ -8,74 +8,105 @@ type TimelineEventProps = {
   setShowUpdateTimelineModal: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
+const HEIGHT = 24;
+
 export default function TimelineEvent({
   timelineEvent,
   updateTimelineRef,
   setShowUpdateTimelineModal,
 }: TimelineEventProps) {
-  console.log('TimelineEvent', timelineEvent);
+  const {timelinePositions, widestTimelineOrdinary} = getTimelinePosition(
+    Number(timelineEvent.startHour),
+    Number(timelineEvent.startMinute),
+    Number(timelineEvent.endHour),
+    Number(timelineEvent.endMinute),
+    timelineEvent.executionTime,
+  );
 
-  const height = 24;
+  return timelinePositions.map((timelinePosition, index) => (
+    <Pressable
+      key={index}
+      onPress={() => {
+        updateTimelineRef.current = timelineEvent;
+        setShowUpdateTimelineModal(true);
+      }}
+      style={{
+        position: 'absolute',
+        top: timelinePosition.top,
+        height: HEIGHT,
+        left: `${timelinePosition.left}%`,
+        width: `${timelinePosition.width}%`,
+        borderColor: timelineEvent.todoColor,
+        borderLeftWidth: index === 0 ? 1 : 0,
+        borderRightWidth: index + 1 === timelinePositions.length ? 1 : 0,
+        marginLeft: 1,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+      }}>
+      <View
+        style={{
+          backgroundColor: timelineEvent.todoColor,
+          opacity: 0.5,
+          flex: 1,
+        }}>
+        {index + 1 === widestTimelineOrdinary && (
+          <Text>{timelineEvent.todoTitle}</Text>
+        )}
+      </View>
+    </Pressable>
+  ));
+}
 
-  const viewCount =
-    (timelineEvent.endHour % 24) - (timelineEvent.startHour % 24) + 1;
+function getTimelinePosition(
+  startHour: number,
+  startMinute: number,
+  endHour: number,
+  endMinute: number,
+  executionTime: number,
+) {
+  const timelinePositions = [];
+  let widestTimelineOrdinary = 1;
+  let widestWidth = 0;
 
-  function calculateWidth(i: number): number {
-    if (viewCount === 1) {
-      // 초 단위라서 분 단위로 바꿔줌
-      return timelineEvent.executionTime / 60;
+  const viewCount = (() => {
+    // 다음날로 넘어가면 1시부터 시작하기때문에 24로 나눔
+    const endHourStartHourSubtract = (endHour % 24) - (startHour % 24) + 1;
+    if (endMinute === 0) {
+      return endHourStartHourSubtract - 1;
     }
-    if (i === 1) {
-      return 60 - timelineEvent.startMinute;
+    return endHourStartHourSubtract;
+  })();
+
+  for (let i = 1; i <= viewCount; i++) {
+    const width = (() => {
+      if (viewCount === 1) {
+        // 초 단위라서 분 단위로 바꿔줌
+        return (executionTime / 60) * 1.5;
+      }
+      if (i === 1) {
+        return (60 - startMinute) * 1.5;
+      }
+
+      if (i === viewCount) {
+        return endMinute * 1.5;
+      }
+
+      return 60 * 1.5;
+    })();
+
+    const top =
+      startHour + i >= 6
+        ? (startHour + i - 6) * HEIGHT + (startHour + i - 6)
+        : (startHour + i + 18) * HEIGHT + (startHour + i + 18);
+    const left = i === 1 ? startMinute * 1.5 + 10 : 10;
+
+    if (widestWidth < width) {
+      widestWidth = width;
+      widestTimelineOrdinary = i;
     }
 
-    if (i === viewCount) {
-      return timelineEvent.endMinute;
-    }
-
-    return 60;
+    timelinePositions.push({width, top, left});
   }
 
-  const generateTimeTableData = () => {
-    console.log('색깔 왜 안나옴', timelineEvent.todoColor);
-    let timeTableData = [];
-
-    //다음날로 넘어갈 수도 있으니까 24로 나눔
-    for (let i = 1; i <= viewCount; i++) {
-      const numberStartHour = Number(timelineEvent.startHour);
-      const top =
-        numberStartHour + i >= 6
-          ? (numberStartHour + i - 6) * height + (numberStartHour + i - 6)
-          : (numberStartHour + i + 18) * height + (numberStartHour + i + 18);
-      const width = calculateWidth(i) * 1.5;
-      const left = i === 1 ? timelineEvent.startMinute * 1.5 + 10 : 10;
-
-      timeTableData.push(
-        <Pressable
-          key={timelineEvent.idx + '' + i}
-          onPress={() => {
-            updateTimelineRef.current = timelineEvent;
-            setShowUpdateTimelineModal(true);
-          }}
-          style={{
-            position: 'absolute',
-            top: top,
-            height: height,
-            left: `${left}%`,
-            width: `${width}%`,
-            backgroundColor: timelineEvent.todoColor,
-            marginLeft: 1,
-            opacity: 0.7,
-            borderColor: timelineEvent.todoColor,
-            // borderWidth: 1,
-          }}>
-          <Text>{timelineEvent.todoTitle}</Text>
-        </Pressable>,
-      );
-    }
-
-    return timeTableData;
-  };
-
-  return generateTimeTableData();
+  return {timelinePositions, widestTimelineOrdinary};
 }
